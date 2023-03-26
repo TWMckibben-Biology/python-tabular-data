6fbbf9a7cb5045eba98b0180d4e4bb4e2267b10e
TaylorMckibben <twm0009@auburn.edu>
2023-03-25 21:55:15 -0500

    Added more code from Jamie's examples. Fixed more syntax, added an arg for the regression plot section.

--- tabulardata.py
+++ tabulardata.py
@@ -12,20 +12,29 @@ def data_regression_plot(data, *argv):
 
 
 def data_plot(data, *argv):
-
     dataframe=pd.read_csv(data)
-
-#Credit: Jamie Oaks
-for arg in argv:
-    dataframe = pd.read_csv("iris.csv")
-    plt.scatter(dataframe.petal_length_cm, dataframe.sepal_length_cm)
-    plt.xlabel("Petal length (cm)")
-    plt.ylabel("Sepal length (cm)")
-    plt.savefig("petal_v_sepal_length.png")
-    plt.clf()
+#Credit: Jamie Oaks, joaks1/python-tabular-data
+    for arg in argv:
+        plt.scatter(dataframe.petal_length_cm, dataframe.sepal_length_cm)
+        plt.xlabel("Petal length (cm)")
+        plt.ylabel("Sepal length (cm)")
+        plt.savefig("petal_v_sepal_length.png")
+        plt.clf()
 
 def regression_plot(data, *argv):
+#Credit: Jamie Oaks, joaks1/python-tabular-data
     dataframe = pd.read_csv(data)
-    
+    for arg in argv:
+        x = dataframe.petal_length_cm
+        y = dataframe.sepal_length_cm
+        regression = stats.linregress(x, y)
+        slope = regression.slope
+        intercept = regression.intercept
+        plt.scatter(x, y, label = 'Data')
+        plt.plot(x, slope * x + intercept, color = "orange", label = 'Fitted line')
+        plt.xlabel("Petal length (cm)")
+        plt.ylabel("Sepal length (cm)")
+        plt.legend()
+        plt.savefig("petal_v_sepal_length_regress.png")
 
 __name__=='__main__'
