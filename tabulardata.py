60b7fc8c3afb915f68053fcb21cd59df430cc1d0
TaylorMckibben <twm0009@auburn.edu>
2023-03-25 21:46:37 -0500

    Fixed module references for main function. Added arg in argv loop

--- tabulardata.py
+++ tabulardata.py
@@ -7,19 +7,25 @@ import scipy
 from scipy import stats
 
 def data_regression_plot(data, *argv):
-    data = pd.read_csv("iris.csv")
-    print("iris.csv")
-    plot_data(data, *argv)
-    regression_plots(data, *argv)
+    data_plot(data, *argv)
+    regression_plot(data, *argv)
 
 
-def mat_plot(data, *argv):
+def data_plot(data, *argv):
+
+dataframe=pd.read_csv(data)
+
 #Credit: Jamie Oaks
+for arg in argv:
     dataframe = pd.read_csv("iris.csv")
     plt.scatter(dataframe.petal_length_cm, dataframe.sepal_length_cm)
     plt.xlabel("Petal length (cm)")
     plt.ylabel("Sepal length (cm)")
     plt.savefig("petal_v_sepal_length.png")
+    plt.clf()
 
+def regression_plot(data, *argv)
+    dataframe = pd.read_csv(data)
+    
 
 __name__=='__main__'
