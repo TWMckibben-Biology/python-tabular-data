4bd2e55998b550a5bf4db976edd6a5d0dd6538b0
TaylorMckibben <twm0009@auburn.edu>
2023-03-25 21:33:02 -0500

    started defining functions

--- tabulardata.py
+++ tabulardata.py
@@ -6,4 +6,9 @@ import matplotlib.pyplot as plt
 import scipy
 from scipy import stats
 
+def data_regression_plot(data, *argv):
+    plot_data(data, *argv)
+    regression_plots(data, *argv)
 
+
+__name__=='__main__'
