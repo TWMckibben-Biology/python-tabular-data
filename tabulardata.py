c38000444fb9fb80505b424ded624bdc1dcaefdc
TaylorMckibben <twm0009@auburn.edu>
2023-03-25 21:40:58 -0500

    Added Jamie's code for using matplot

--- tabulardata.py
+++ tabulardata.py
@@ -7,8 +7,19 @@ import scipy
 from scipy import stats
 
 def data_regression_plot(data, *argv):
+    data = pd.read_csv("iris.csv")
+    print("iris.csv")
     plot_data(data, *argv)
     regression_plots(data, *argv)
+
+
+def mat_plot(data, *argv):
+#Credit: Jamie Oaks
+    dataframe = pd.read_csv("iris.csv")
+    plt.scatter(dataframe.petal_length_cm, dataframe.sepal_length_cm)
+    plt.xlabel("Petal length (cm)")
+    plt.ylabel("Sepal length (cm)")
+    plt.savefig("petal_v_sepal_length.png")
 
 
 __name__=='__main__'
